2c82f3d743e095bad05ef28aefbceb5481b412c5
Aman Joshi <amanjoshi668@gamil.com>
2018-11-23 18:36:12 +0530

    Md5 function modified

--- pastryClient/test.cpp
+++ pastryClient/test.cpp
@@ -1,1 +1,25 @@
 #include <iostream>
+#include <string>
+// #include "../include/md5.h"
+#include <openssl/md5.h>
+using namespace std;
+int main(){
+    string bytes = "hello";
+    int len = 2;
+    unsigned char hash_buff[MD5_DIGEST_LENGTH];
+    MD5((const unsigned char *)bytes.c_str(), bytes.size(), hash_buff);
+    string nodeID = "";
+    auto n = *(unsigned long long *)hash_buff;
+    for (int i = 0; i < 64; i += len)
+    {
+        int res = 0;
+        for (int j = 0; j < len; j++)
+        {
+            res *= 2;
+            if (n&(1<<(i+j))) res++;
+        }
+        nodeID += char(res+'0');
+    }
+    cout<<nodeID<<endl;
+    return 0;
+}
